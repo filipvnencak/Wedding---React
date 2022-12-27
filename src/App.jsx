192574e7a65ecf6605e81659c5e15441f5d46067
filipvnencak <vnencak.filip@gmail.com>
2022-12-27 17:09:42 +0100

    Do staff

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import loveImage from "./assets/svgs/undraw_love_is_in_the_air_re_lp58.svg";
 import UseCountdown from "./CountDown";
-import Game from "./seat";
+import Game from "./game";
 export default function App() {
   return (
     <div className="App">
@@ -10,12 +10,14 @@ export default function App() {
           Radka& <br />
           Filip
         </h1>
-        <Game />
         <img src={loveImage} alt="love" />
       </section>
       <section>
         <h2 id="date">01 | 07 | 23</h2>
         <UseCountdown />
+      </section>
+      <section>
+        <Game />
       </section>
     </div>
   );
